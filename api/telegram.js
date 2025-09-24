11dc4e15ee95665dc707f505001c011011b31667
Alejo Yornet <49294158+alejoy@users.noreply.github.com>
2025-09-24 16:43:49 -0300

    Create telegram.js

---
+++ api/telegram.js
@@ -0,0 +1,15 @@
+let estadoTimbre = require("./estadoTimbre"); // Compartir estado en memoria
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { message } = req.body;
+
+    if (message && message.text) {
+      estadoTimbre.mensajes.push({ de: "admin", texto: message.text });
+    }
+
+    return res.status(200).send("OK");
+  }
+
+  return res.status(405).send("Método no permitido");
+}
