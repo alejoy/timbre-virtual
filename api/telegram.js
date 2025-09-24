88333d7d9892efe08cbaf661f8489ceae16d2de6
Alejo Yornet <49294158+alejoy@users.noreply.github.com>
2025-09-24 16:59:18 -0300

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -3,10 +3,14 @@ import { estadoTimbre } from "./estadoTimbre";
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { message } = req.body;
+
     if (message && message.text) {
       estadoTimbre.mensajes.push({ de: "admin", texto: message.text });
+      console.log("Nuevo mensaje admin:", message.text);
     }
+
     return res.status(200).send("OK");
   }
+
   return res.status(405).send("Método no permitido");
 }
